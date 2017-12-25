a13d8e27a65aca43c0b425ade76c54fd0839f2f6
angelusreseda <angelus.reseda@gmail.com>
2017-12-25 14:56:13 +0100

    qwe
    qwe

--- config/rbgtracker.js
+++ config/rbgtracker.js
@@ -1,4 +1,4 @@
 // config/rbgtracker.js
 module.exports.rbgtracker = {
-  baseUrl: 'https://eu.api.battle.net/wow/character/tarren-mill/sahrvin?fields=pvp&locale=en_GB&apikey=4yd45k8adzp3nf2am6kc4g7nn3r73xv3'
+  baseUrl: 'https://eu.api.battle.net/wow/character/tarren-mill/sarvyn?fields=pvp&locale=en_GB&apikey=4yd45k8adzp3nf2am6kc4g7nn3r73xv3'
 };
